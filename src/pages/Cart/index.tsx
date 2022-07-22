239c08d8ee0dbc7d790d4ac61d605419ae05450b
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-07-22 03:30:50 -0300

    feat: increase/decrease a product amount

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -32,11 +32,17 @@ const Cart = (): JSX.Element => {
   );
 
   function handleProductIncrement(product: Product) {
-    // TODO
+    updateProductAmount({
+      productId: product.id,
+      amount: product.amount + 1,
+    });
   }
 
   function handleProductDecrement(product: Product) {
-    // TODO
+    updateProductAmount({
+      productId: product.id,
+      amount: product.amount - 1,
+    });
   }
 
   function handleRemoveProduct(productId: number) {
@@ -109,7 +115,7 @@ const Cart = (): JSX.Element => {
 
         <Total>
           <span>TOTAL</span>
-          <strong>R$ 359,80</strong>
+          <strong>{total}</strong>
         </Total>
       </footer>
     </Container>
